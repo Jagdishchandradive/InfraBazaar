165a061bd535e2415e640be523ca0cecaf9bacec
JagdishchandraDive <jagdishchandradive@gmail.com>
2023-10-12 11:19:52 +0530

    Initial Commit.

---
+++ Infra-Bazaar/src/main/java/com/info/Infra_Bazaar_Application.java
@@ -0,0 +1,14 @@
+package com.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Infra_Bazaar_Application
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Infra_Bazaar_Application.class, args);
+	}
+}
+
